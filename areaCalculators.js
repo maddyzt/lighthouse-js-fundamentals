21fb7c6d82eb2d8639643ba6bfd8b0c13d43e424
Maddy Tan <maddytan7@gmail.com>
2022-04-12 15:17:39 +0000

    area calculators challenge

---
+++ areaCalculators.js
@@ -0,0 +1,32 @@
+function calculateRectangleArea(length, width){
+  if(length >= 0 && width >= 0){
+    const rectangleArea = length * width
+    return rectangleArea
+  }
+}
+
+function calculateTriangleArea(base, height){
+  if(base >= 0 && height >= 0){
+    const triangleArea = base * height / 2
+    return triangleArea
+  }
+}
+
+function calculateCircleArea(radius){
+  if(radius >= 0){
+    const circleArea = Math.PI * radius * radius
+    return circleArea
+  }
+}
+
+console.log(calculateRectangleArea(10, 5));     // should print 50
+console.log(calculateRectangleArea(1.5, 2.5));  // should print 3.75
+console.log(calculateRectangleArea(10, -5));    // should print undefined
+
+console.log(calculateTriangleArea(10, 5)); // should print 25
+console.log(calculateTriangleArea(3, 2.5)); // should print 3.75
+console.log(calculateTriangleArea(10, -5)); // should print undefined
+
+console.log(calculateCircleArea(10)); // should print 314.159...
+console.log(calculateCircleArea(3.5)); // should print 38.484...
+console.log(calculateCircleArea(-1)); // should print undefined
