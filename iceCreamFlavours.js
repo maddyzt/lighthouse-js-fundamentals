a6c3a4276df4143281957957cf0cf9de674659a7
Maddy Tan <maddytan7@gmail.com>
2022-04-12 19:53:14 +0000

    iceCreamFlavours.js

---
+++ iceCreamFlavours.js
@@ -0,0 +1,7 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[4]);
+console.log(iceCreamFlavours.length);
