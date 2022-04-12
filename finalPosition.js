f84ef5ed100e5cfdeaa01f1bc0e6a0d417f55c32
Maddy Tan <maddytan7@gmail.com>
2022-04-12 14:57:34 +0000

    final position challenge

---
+++ finalPosition.js
@@ -0,0 +1,23 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']  
+
+function finalPosition(){
+  const finish = []
+  let x = 0
+  let y = 0
+  for (const move of moves){
+    if (move === 'north'){
+      y = y + 1
+    } 
+    else if (move === 'south'){
+      y = y - 1
+    }
+    else if (move === 'west'){
+      x = x - 1
+    }
+    else if (move === 'east'){
+      x = x + 1
+    }
+  }
+return [x, y]
+}
+finalPosition(moves);
